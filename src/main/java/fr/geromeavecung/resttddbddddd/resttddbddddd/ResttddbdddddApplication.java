c25755a14deaf753139f8548707e67174c914c9c
gerom <gerome.parmentier@gmail.com>
2024-01-12 09:54:02 +0100

    spring initializr

---
+++ src/main/java/fr/geromeavecung/resttddbddddd/resttddbddddd/ResttddbdddddApplication.java
@@ -0,0 +1,13 @@
+package fr.geromeavecung.resttddbddddd.resttddbddddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttddbdddddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttddbdddddApplication.class, args);
+	}
+
+}
