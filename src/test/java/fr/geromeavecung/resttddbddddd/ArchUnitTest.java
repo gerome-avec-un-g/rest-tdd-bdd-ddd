558dcbd6477c20b9e93c005d2a953cd493ebfa95
gerome <gerome.parmentier@gmail.com>
2024-06-25 10:54:45 +0200

    archunit

--- src/test/java/fr/geromeavecung/resttddbddddd/ArchUnitTest.java
+++ src/test/java/fr/geromeavecung/resttddbddddd/ArchUnitTest.java
@@ -9,57 +9,58 @@ import org.junit.jupiter.api.Test;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 class ArchUnitTest {
-    
+
+    public static final JavaClasses CLASSES = new ClassFileImporter().importPackages("fr.geromeavecung.resttddbddddd");
+
     @Test
     void domain_cant_depend_on_drivers() {
-        JavaClasses classes = new ClassFileImporter().importPackages("fr.geromeavecung.resttddbddddd");
-
         ArchRule rule = noClasses().that().resideInAPackage("..domain..")
                 .should().dependOnClassesThat().resideInAPackage("..drivers..");
-    
-        rule.check(classes);
+
+        rule.check(CLASSES);
     }
 
     @Test
     void domain_cant_depend_on_clients() {
-        JavaClasses classes = new ClassFileImporter().importPackages("fr.geromeavecung.resttddbddddd");
-
         ArchRule rule = noClasses().that().resideInAPackage("..domain..")
                 .should().dependOnClassesThat().resideInAPackage("..clients..");
 
-        rule.check(classes);
+        rule.check(CLASSES);
     }
 
     @Test
     void clients_cant_depend_on_drivers() {
-        JavaClasses classes = new ClassFileImporter().importPackages("fr.geromeavecung.resttddbddddd");
-
         ArchRule rule = noClasses().that().resideInAPackage("..clients..")
                 .should().dependOnClassesThat().resideInAPackage("..drivers..");
 
-        rule.check(classes);
+        rule.check(CLASSES);
     }
 
     @Test
     void bounded_contexts_cant_depend_on_use_cases() {
-        JavaClasses classes = new ClassFileImporter().importPackages("fr.geromeavecung.resttddbddddd");
-
         ArchRule rule = noClasses().that().resideInAPackage("..boundedcontexts..")
                 .should().dependOnClassesThat().resideInAPackage("..usecases..");
 
-        rule.check(classes);
+        rule.check(CLASSES);
     }
 
     // TODO use cases can't depend on DDD repository
     @Test
-    void cucumber_steps_annotations_cannot_be_and() {
-        JavaClasses classes = new ClassFileImporter().importPackages("fr.geromeavecung.resttddbddddd");
-
+    void cucumber_steps_annotations_cant_be_and() {
         ArchRule rule = noClasses().should().dependOnClassesThat()
                 .haveFullyQualifiedName("io.cucumber.java.en.And");
 
-        rule.check(classes);
+        rule.check(CLASSES);
     }
+
+    // does not work, ArchUnit only checks bytecode...
+//    @Test
+//    void lombok_cant_be_used() {
+//        ArchRule rule = noClasses().should().dependOnClassesThat()
+//                .resideInAPackage("lombok..");
+//
+//        rule.check(CLASSES);
+//    }
 
 
 }
