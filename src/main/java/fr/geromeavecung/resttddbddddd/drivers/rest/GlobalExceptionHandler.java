723e7e6d7b1a3ce265e2b8d01e14620fe1b7a590
gerome <gerome.parmentier@gmail.com>
2025-07-21 21:02:20 +0200

    error management

--- src/main/java/fr/geromeavecung/resttddbddddd/drivers/rest/GlobalExceptionHandler.java
+++ src/main/java/fr/geromeavecung/resttddbddddd/drivers/rest/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package fr.geromeavecung.resttddbddddd.drivers.rest;
 
 import fr.geromeavecung.resttddbddddd.domain.boundedcontexts.shared.ValidationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +11,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
     @ExceptionHandler({ValidationException.class})
     public ResponseEntity<ProblemDetail> handleValidationException(final ValidationException exception) {
-        LOGGER.info(exception.getMessage());
+        logger.info(exception.getMessage());
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
         return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<ProblemDetail> handleException(final Exception exception) {
-        LOGGER.error(exception.getMessage());
+        logger.error(exception.getMessage());
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
         return new ResponseEntity<>(problemDetail, HttpStatus.INTERNAL_SERVER_ERROR);
     }
